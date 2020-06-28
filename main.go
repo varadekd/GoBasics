79c9fa9248f24fbe076c1a8714753323688996db
Varadekushagra <kushagra@robustest.com>
2020-06-28 14:51:35 +0530

    Inital commit

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+
+	"../src/methods"
+)
+
+var globalVariable int = 10
+
+//var globalVariable string = 90
+// The above declartion will not work since we are explicitly mentioned that indentifer should of type string and we are passing value int
+
+// var globalVariable = 70 This is also a correct way fo declaring the variable here we are not specifying the type hence it will detect on our own
+
+// globalVariable := 90 Why this declaration is not valid?
+
+func main() {
+	// Printing message for package is called
+	methods.PackageImportMessage()
+	// methods.packageImportMessage(globalVariable) this will throw error saying too many aruguments are passed. Because function is
+	// No arguments and vice versa is also possible that when function is expecting something and we are not passing anything
+
+	// Why this throws an error
+	// const constantValue = "I am string"
+	// methods.ChangeStringValue(&constantValue)
+
+	const divisor = 2
+
+	// This is a wrong way to access the package methods
+	// methods.packageImportMessage()
+	// Reason is we are trying to access the un exported function
+
+	fmt.Println("\nValue of globalVariable before calling NoChangeInValue : ", globalVariable)
+	methods.NoChangeInValue(&globalVariable)
+	fmt.Println("Value of globalVariable before calling NoChangeInValue : ", globalVariable)
+	// The reason in Go for every new variable that is declared a dedicated for variable so when we are assinging and then changing
+	// The value of new variable so when we changed the new value it will relfect/change the value for that particular address
+
+	fmt.Println("\nValue of globalVariable before calling ChangeValue: ", globalVariable)
+	// I am passing the address where the globalVariable is sotered inside the memory.
+	// If we are passing anything other than type int it will throw error reason being we are expecting the value to of type int
+	methods.ChangeValue(&globalVariable)
+	fmt.Println("Value of globalVariable after calling ChangeValue: ", globalVariable)
+	// Passing the address will make sense only in the cases where I want to change the value stored on that address.
+
+	// Example to access the value and then returing the computed values
+	fmt.Println("\nWill return the sqaure of the value passed: ", methods.GetSqaure(globalVariable))
+
+	// However changing value of the variable is also possible by returning a value from the function and then assinging it
+	globalVariable = methods.ChangeValueThroughReturn()
+	fmt.Println("\nValue of globalVariable after calling ChangeValueThroughReturn: ", globalVariable)
+	// globalVariable := methods.ChangeValueThroughReturnWithValue(globalVariable) Explation is file
+	// Why this cannot be used here
+
+	// Functions returning multiple things
+	remainder, err := methods.GetDivisionValue(globalVariable, divisor)
+	fmt.Println("The value after divison is: ", remainder, err)
+
+	// globalVariable = methods.ChangeValueThroughReturnString()
+	// This will throw an error becase though the function is returning the valid value but the variable has a value of type int
+
+	// The below part is valid since we are declaring a new variable and in case it won't show any error
+	value := methods.ChangeValueThroughReturnString()
+	fmt.Println("\nChangeValueThroughReturnString returned the value: ", value)
+}
---
+++ methods/methods.go
@@ -0,0 +1,78 @@
+package methods
+
+import (
+	"errors"
+	"fmt"
+)
+
+func PackageImportMessage() {
+	fmt.Println("Package components are properly imported")
+}
+
+// func packageImportMessage() {
+// 	fmt.Println("Package components are properly imported")
+// }
+// The above function declaration is also valid but we can't use this outside the package since it is not exported
+// How ever if you want to use it we can use in the way commented below
+// func ChangeValue(a *int) {
+// 	packageImportMessage()
+// 	*a = 90
+// }
+
+func NoChangeInValue(a *int) {
+	val := *a
+	val = 80
+	fmt.Println("Value changed to: ", val)
+}
+
+func ChangeValue(a *int) {
+	// *a this helps us to us to access the value that is stored at the passed address
+	*a = 90
+}
+
+func ChangeValueThroughReturn() int {
+	return 231
+}
+
+func GetSqaure(val int) int {
+	return val * val
+
+	// Can be done in this fashion also but I will pefer the above way because In this context I have to return sqaure so not storing this value
+	// newVal = val * val
+	// return newVal
+	// OR like this we arer squaring the passed val and storing the same inside the passed value and then returning it
+	// val = val * val
+	// return val
+}
+
+// This is also valid but I will not pefer this because my intention is to return value without performing any operation on the inital value
+// So there is no point in passing the value where the motive is to change the value
+// func ChangeValueThroughReturnWithValue(a int) int {
+// 	a = 200
+// 	return a
+// }
+
+// The below one is also value we are returning a value of type string
+func ChangeValueThroughReturnString() string {
+	return "This is a string"
+}
+
+func ChangeStringValue(s *string) {
+	*s = "The string is changed"
+}
+
+// However this is not valid
+// func ChangeValueThroughReturnString() string {
+// 	return 230
+// }
+// Reason for this is we are saying that the function will return a value of type string and in actual we are returning a value of type int
+
+func GetDivisionValue(dividend, divisor int) (int, error) {
+	if divisor == 0 {
+		return 0, errors.New("The value of divisor cannot be zero")
+	}
+
+	return (dividend / divisor), nil
+}
+
+// Since the input of two int is fiven as int inorder to return float we need to convert it
