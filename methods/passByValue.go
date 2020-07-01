a2d21a54358e48f63dd3b0fd0a7f3a5f81acd27d
Varadekushagra <kushagra@robustest.com>
2020-07-01 14:01:40 +0530

    P{ass by value is added

---
+++ methods/passByValue.go
@@ -0,0 +1,8 @@
+package methods
+
+import "fmt"
+
+// The purpose of this program is to understand the learning of pass by value
+func PassingValue(a *int) {
+	fmt.Println("Checking the address for this location: ", a)
+}
