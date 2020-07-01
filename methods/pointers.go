117a16e24edd09c4f297159fc1cb1d806565fd24
Varadekushagra <kushagra@robustest.com>
2020-07-01 17:04:16 +0530

    Concepts of pointer is added

--- methods/pointers.go
+++ methods/pointers.go
@@ -9,11 +9,14 @@ func UnderstandingPointer() {
 
 	i := 10 // Short hand declaration for variable i because we are not bother about the int type means int, int8
 	// var i int = 10 also valid declaration
+	fmt.Println("\nValue of i: ", i)
 
 	//i1 *int := &i We can't declare this it is not valid because the short hand declaration did not expect the type to be declared,
 	// It will just check for identifer name to the left side
 
 	i1 := &i
+	changeValue(i1)
+	fmt.Println("Value of i: ", i)
 	//var i1 *int = &i
 	// i1 := &i and var i1 *int = &i both are valid declaration because if we are not specifying the type it will take on this own
 	// if *int is not defined then it will throw error reason because it is looking for a refrence to connect to that location
@@ -36,5 +39,5 @@ func UnderstandingPointer() {
 
 func changeValue(a *int) {
 	fmt.Println("Passed argument values: ", a, &a, *a)
-	*a = 20
+	*a = *a + 10
 }
