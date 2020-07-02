d41d760b0f1161d63b48603245c6b10079306168
Varadekushagra <kushagra@robustest.com>
2020-07-02 21:16:28 +0530

    Same method declaration

--- methods/declarationOfSameMethod.go
+++ methods/declarationOfSameMethod.go
@@ -1,10 +1,13 @@
 package methods
 
-// The purpose of this program is to declatre two methods of same name this won't work because the same function name is redeclared
-// func PrintPassedValue(a int) {
-// 	fmt.Printf("Passed value is %d of type %T", a, a)
-// }
+import "fmt"
 
-// func PrintPassedValue(a float32) {
-// 	fmt.Printf("Passed value is %f of type %T", a, a)
-// }
+// The purpose of this program is to declatre two methods of same name
+
+func PrintPassedValue(a *int) {
+	fmt.Printf("Passed value is %d of type %T", a, a)
+}
+
+func PrintPassedValue(a *float32) {
+	fmt.Printf("Passed value is %f of type %T", a, a)
+}
